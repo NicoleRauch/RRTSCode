15e2e3b7fab505b268bfc08bf9c505ec0c3a975e
Till Rauch <till.rauch@me.com>
2019-03-10 12:24:29 +0100

    test-solution-async-calls uebersetzt

--- test-solution-async-calls/endToEndTest.tsx
+++ test-solution-async-calls/endToEndTest.tsx
@@ -1,15 +1,14 @@
 import "jsdom-global/register";
 import React from "react";
 import expect from "must";
-import sinon from "sinon";
-import {mount} from "enzyme";
-import {applyMiddleware, createStore} from "redux";
+import sinon, {SinonFakeServer, SinonFakeXMLHttpRequestStatic} from "sinon";
+import {mount, ReactWrapper} from "enzyme";
+import {applyMiddleware, createStore, Store} from "redux";
 import {Provider} from "react-redux";
 import thunkMiddleware from "redux-thunk";
 
 import reducers from "../src-solution-async-calls/reducers";
 import App from "../src-solution-async-calls/App";
-import UserList from "../src-solution-redux-1/UserList";
 
 const users = [
   {firstName: "Nicole", lastName: "Rauch"},
@@ -17,39 +16,44 @@ const users = [
   {firstName: "Fritz", lastName: "Walter"}
 ];
 
+declare var global: {XMLHttpRequest : SinonFakeXMLHttpRequestStatic};
+
 describe("Username end2end test", function () {
+  let server : SinonFakeServer;
+  let store : Store;
+  let component: ReactWrapper;
 
   beforeEach(function () {
     global.XMLHttpRequest = sinon.FakeXMLHttpRequest;
-    this.server = sinon.fakeServer.create({respondImmediately: true});
+    server = sinon.fakeServer.create({respondImmediately: true});
 
-    this.store = createStore(reducers,
+    store = createStore(reducers,
       applyMiddleware(thunkMiddleware)
     );
   });
 
   afterEach(function () {
-    this.server.restore();
+    server.restore();
   });
 
   it("displays all loaded users", function () {
-    this.server.respondWith("GET", "/api/users",
+    server.respondWith("GET", "/api/users",
       [200, {"Content-Type": "application/json"}, JSON.stringify(users)]
     );
-    this.server.respondWith("POST", "/api/user",
+    server.respondWith("POST", "/api/user",
       [200, {"Content-Type": "application/json"}, "User successfully added."]
     );
 
     // In case you need to find out which API is called, enable this:
     // this.server.respondWith(response => { console.log(response.url); response.respond(); });
 
-    this.component = mount(<Provider store={this.store}>
+    component = mount(<Provider store={store}>
         <App/>
       </Provider>
     );
 
     // you can interact with the UI here
 
-    expect(this.component.find("span").map(u => u.text())).to.eql(["Nicole","Rauch","Peter","Müller","Fritz","Walter"]);
+    expect(component.find("span").map(u => u.text())).to.eql(["Nicole","Rauch","Peter","Müller","Fritz","Walter"]);
   });
 });
