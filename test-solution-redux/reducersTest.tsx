e70ee0dd75d1fc9dfb5830e59b26f7368aabca81
Nicole Rauch <info@nicole-rauch.de>
2020-07-18 11:16:28 +0200

    Formatierung, Typisierung.

--- test-solution-redux/reducersTest.tsx
+++ test-solution-redux/reducersTest.tsx
@@ -1,31 +1,34 @@
 
 import reducer, {addUser, UserActions} from "../src-solution-redux-4/reducers";
-import {IUser} from "../src-solution-react/types";
+import {IUser, StoreState} from "../src-solution-redux-4/types";
 
 
 
 const user: IUser = {firstName: "firstName", lastName: "lastName"};
 const stateUserOne: IUser = {firstName: "Peter", lastName: "Klaus"};
-const stateUserTwo: IUser = {firstName: "Max", lastName: "Mustermann"}
+const stateUserTwo: IUser = {firstName: "Max", lastName: "Mustermann"};
 
 describe('reducers', () => {
 
   describe('Actions', () => {
     it('creates a USER_ADDED action', () => {
-      expect(addUser(user)).toEqual({type: UserActions.USER_ADDED, user});
+      expect(addUser(user))
+          .toEqual({type: UserActions.USER_ADDED, user});
     });
   });
 
-  describe('reducer function', () => {
-    it('adds a user when there are no users yet', () => {
-      const EMPTY_STATE = {users: []};
-      const newState = reducer(EMPTY_STATE, addUser(user));
-      expect(newState).toEqual({users: [user]});
-    });
-    it('adds a user when there are already users in the state', () => {
-      const NONEMPTY_STATE = {users: [stateUserOne, stateUserTwo]};
-      const newState = reducer(NONEMPTY_STATE, addUser(user));
-      expect(newState).toEqual({users: [stateUserOne, stateUserTwo, user]});
-    });
+describe('reducer function', () => {
+  it('adds a user when there are no users yet', () => {
+    const EMPTY_STATE: StoreState = {users: []};
+    const newState = reducer(EMPTY_STATE, addUser(user));
+    expect(newState).toEqual({users: [user]});
   });
+  it('adds a user when there are already users in the state', () => {
+    const NONEMPTY_STATE: StoreState =
+        {users: [stateUserOne, stateUserTwo]};
+    const newState = reducer(NONEMPTY_STATE, addUser(user));
+    expect(newState)
+        .toEqual({users: [stateUserOne, stateUserTwo, user]});
+  });
+});
 });
