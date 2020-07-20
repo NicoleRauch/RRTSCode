96e1be1e7c5c13268d8871efdaf8afd4959aa14a
Nicole Rauch <info@nicole-rauch.de>
2020-07-20 23:15:22 +0200

    Formatierung für die Folien.

--- src-solution-hooks/UserWithHook.tsx
+++ src-solution-hooks/UserWithHook.tsx
@@ -6,20 +6,20 @@ export type UserWithHookProps = {
 }
 
 const UserWithHook =
-    ({user:{firstName, lastName}}: UserWithHookProps): ReactElement => {
+  ({user:{firstName, lastName}}: UserWithHookProps): ReactElement => {
 
-    const [highlighted, setHighlighted] = useState(false);
-    const color = highlighted ? "#FF0000" : "#FFFFFF";
+  const [highlighted, setHighlighted] = useState(false);
+  const color = highlighted ? "#FF0000" : "#FFFFFF";
 
-    return (
-        <div onClick={(): void => {setHighlighted(!highlighted)}}
-             style={{cursor: "pointer"}}>
-            <label>First name: </label>
-            <span style={{backgroundColor: color}}>{firstName}</span><br/>
-            <label>Last name: </label>
-            <span>{lastName}</span><br/>
-        </div>
-    );
+  return (
+    <div onClick={(): void => setHighlighted(!highlighted)}
+       style={{cursor: "pointer"}}>
+      <label>First name: </label>
+      <span style={{backgroundColor: color}}>{firstName}</span><br/>
+      <label>Last name: </label>
+      <span>{lastName}</span><br/>
+    </div>
+  );
 };
 
 export default UserWithHook;
