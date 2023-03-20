dc87caf4faa371cbbe4f6f19b8ac6c5584d50999
Nicole Rauch <info@nicole-rauch.de>
2023-03-20 12:18:46 +0100

    JSX anhand einer Functional Component zeigen.

--- src-solution-react/HelloWorldClassComponentWithoutJSX.tsx
+++
@@ -1,8 +0,0 @@
-import React, {Component, ReactElement} from "react";
-
-export default class HelloWorld extends Component {
-
-    render(): ReactElement {
-        return React.createElement('p', null, 'Hello World!');
-    }
-}
---
+++ src-solution-react/HelloWorldFunctionalComponentWithoutJSX.tsx
@@ -0,0 +1,6 @@
+import React, { ReactElement } from "react";
+
+const HelloWorld = () : ReactElement =>
+    React.createElement('p', null, 'Hello World!');
+
+export default HelloWorld;
